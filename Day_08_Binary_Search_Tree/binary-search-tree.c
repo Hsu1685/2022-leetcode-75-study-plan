822f5ad354bf967c40f7c810e980ae6dc6015ac8
Chihmin <chihmin.hsu.a5u@tw.denso.com>
2022-07-29 15:19:12 +0800

    Update binary-search-tree.c

--- Day_08_Binary_Search_Tree/binary-search-tree.c
+++ Day_08_Binary_Search_Tree/binary-search-tree.c
@@ -1,16 +1,16 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 struct TreeNode {
     int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
- };
- struct TreeNode* newNode(int);
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+struct TreeNode* newNode(int);
 
-void run_inorder(struct TreeNode *);
+void run_inorder(struct TreeNode*);
 struct TreeNode* newNode(int);
 
 int counter;
@@ -31,13 +31,13 @@ int main() {
 
     counter = 0;
     run_inorder(root);
-    printf ("counter: %d\n", counter);
+    printf("counter: %d\n", counter);
     return 0;
 }
 
 void run_inorder(struct TreeNode* ptr) {
     if (ptr != NULL) {
-        run_inorder(ptr->left);  // 先是左子節點
+        run_inorder(ptr->left);       // 先是左子節點
         printf("[%2d]\n", ptr->val);  // 再來是自身
         counter++;
         run_inorder(ptr->right);  // 最後是右子節點
